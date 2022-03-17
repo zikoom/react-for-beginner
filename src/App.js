3a3f86ab75711bd012eb71d4b1d0fa757665392d
yoon ji hoon <koom@utplus.co.kr>
2022-03-17 23:55:58 +0900

    컴포넌트 파괴될 때 함수 실행시키기~

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import './App.css';
+import "./App.css";
 
-import Button from './Button';
-import styles from './App.module.css';
+import Button from "./Button";
+import styles from "./App.module.css";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hello");
+    return () => {
+      console.log("bye");
+    };
+  }, []);
+  return <h1>haha</h1>;
+}
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => {
-    setValue((prev) => prev + 1);
-  };
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-  console.log('run all the time');
-  useEffect(() => {
-    console.log('CALL API...');
-  }, []);
-
-  useEffect(() => {
-    console.log('search ', keyword);
-  }, [keyword]);
+  const [isShoing, setIsShoing] = useState(false);
+  const onClick = () => setIsShoing((prev) => !prev);
   return (
     <div>
-      <h1 className={styles.title}>Welcom !</h1>
-      <h2>{counter}</h2>
-      <input value={keyword} type="text" placeholder="Search here..." onChange={onChange} />
-      <button onClick={onClick}>click me!</button>
-      <Button text={'click me'} />
-      <Button text={'hoho'} />
+      <button onClick={onClick}>{isShoing ? "hide" : "show"}</button>
+      {isShoing ? <Hello /> : null}
+      <h1
+        style={{
+          display: isShoing ? "block" : "none",
+        }}
+      >
+        hide and seek
+      </h1>
     </div>
   );
 }
