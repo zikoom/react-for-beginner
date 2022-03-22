6de2cfc7356a953f7904d8bf29a2721758ea4a2b
yoon ji hoon <koom@utplus.co.kr>
2022-03-22 22:47:29 +0900

    영화 정보가져와서 보여주기

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ function App() {
   console.log("app lendered");
 
   const [isLoading, setIsLoading] = useState(true);
-  const movieURL = "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year";
-
-  let movieList = [];
+  const [movieList, setMovieList] = useState([]);
+  const movieURL = "https://yts.mx/api/v2/list_movies.json?minimum_rating=9.5&sort_by=year";
 
   useEffect(async () => {
-    movieList = await (await fetch(movieURL)).json();
-    movieList = movieList.data.movies;
+    let response = await (await fetch(movieURL)).json();
+    response = response.data.movies;
 
-    console.log("받아온 영화 데이터: ", movieList);
+    console.log("받아온 영화 데이터: ", response);
+    setMovieList(() => response.map((movie) => movie));
     setIsLoading(false);
   }, []);
 
   return (
     <div>
       <h1>Movies!</h1>
-      {isLoading ? <strong> 로딩중 ...</strong> : <Movie />}
+      {isLoading ? <strong> 로딩중 ...</strong> : <Movie movieList={movieList} />}
     </div>
   );
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,30 @@
+import PropTypes from 'prop-types';
 
-function Movie(){
 
+function Movie({movieList}){
+  console.log('movieList in Movie Component: ', movieList)
   return (
-    <div>~무비 컴포넌트~</div>
+    <div className="movieCon">
+      <div>~무비 컴포넌트~</div>
+      <div>
+        {movieList.map((movie) => {
+          return (
+          <div key={movie.id}>
+            
+            <img src={movie.medium_cover_image} />
+            <h2>{movie.title}</h2>
+            <h4>{movie.genres.map((genre) => <p key={genre}>{genre}</p>)}</h4>
+            <p>{movie.summary}</p>
+          </div>
+        )})}
+      </div>
+    </div>
+
   )
+}
+
+Movie.propTypes = {
+  movieList: PropTypes.array.isRequired,
 }
 
 export default Movie;
